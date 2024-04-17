a2ea828a3138f76402f079f380ff68af66180e9b
Andrea Jao <ndrjao@gmail.com>
2024-04-17 13:06:21 -0700

    cleaned up notes

--- tvmaze.js
+++ tvmaze.js
@@ -9,7 +9,6 @@ const TVMAZE_BASE_URL = `https://api.tvmaze.com/search/shows/`;
 
 async function getShowsByTerm(term) {
   console.log("initializing getShowsByTerm");
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
 
   const paramTerm = new URLSearchParams({ q: term });
   const showFetchURL = TVMAZE_BASE_URL.concat(`?${paramTerm}`);
@@ -34,26 +33,6 @@ async function getShowsByTerm(term) {
   console.log("our final input for show info: ", formattedShowInfo);
   return formattedShowInfo;
 }
-
-//  [
-//   {
-//     id: 1767,
-//     name: "The Bletchley Circle",
-//     summary:
-//       `< p > <b>The Bletchley Circle</b> follows the journey of four ordinary;
-//            women with extraordinary skills that helped to end World War II.</p>
-//          <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-//            normal lives, modestly setting aside the part they played in
-//            producing crucial intelligence, which helped the Allies to victory
-//            and shortened the war. When Susan discovers a hidden code behind an
-//            unsolved murder she is met by skepticism from the police. She
-//            quickly realises she can only begin to crack the murders and bring
-//            the culprit to justice with her former friends.</p>`,
-//     image:
-//       "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-//   }
-// ];
-
 
 
 // ADD: other functions that will be useful for getting episode/show data
